5e39443915ae08b737b8dd8645322c277a2327fd
Reginaldo Costa <regcostajr@gmail.com>
2018-06-30 02:06:46 +0800

    Update setup_essentials.sh

--- setup_essentials.sh
+++ setup_essentials.sh
@@ -5,3 +5,11 @@ sudo apt-get install -y software-properties-common build-essential cpanminus dh-
 
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git//git/master/contrib/completion/git-prompt.sh
 cp .bash_profile ~/.bash_profile
+source ~/.bash_profile
+
+git config --global user.name regcostajr
+git config --global user.email regcostajr@gmail.com
+git config --global color.ui true
+git config --global color.status true
+git config --global color.diff true
+git config --global core.pager "less -r"
