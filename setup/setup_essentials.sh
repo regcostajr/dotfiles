735844d82b589371ad57f2afad2d9dbd943bfe34
Reginaldo Costa <regcostajr@gmail.com>
2021-03-11 17:11:19 +0100

    Update setup_essentials.sh

--- setup/setup_essentials.sh
+++ setup/setup_essentials.sh
@@ -2,30 +2,7 @@
 
 sudo apt-get update
 
-packagelist=(
-    software-properties-common
-    build-essential
-    libperl-dev
-    cpanminus
-    dh-dist-zilla
-    libtest-perl-critic-perl
-    exuberant-ctags
-    git
-    python-dev
-    python3-dev
-    ruby-dev
-    lua5.1
-    liblua5.1-dev
-    dirmngr
-    postgresql-client-9.6
-    nginx
-    jq
-    cmake
-)
-
-for i in ${packagelist[@]}; do
-  sudo apt-get install -y $i
-done
+sudo apt-get install -y software-properties-common build-essential libperl-dev cpanminus dh-dist-zilla libtest-perl-critic-perl exuberant-ctags git python-dev python3-dev ruby-dev lua5.1 liblua5.1-dev dirmngr postgresql-client-9.6 nginx jq cmake
 
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 cat .bash_profile >> ~/.bash_profile
